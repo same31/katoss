c206efd39676da22f716582bbfbda3621ae367b8
same31 <matthieu.mitrani@gmail.com>
2015-11-21 08:27:03 +0100

    search with SickRage API

---
+++ katoss/searchSickRage.js
@@ -0,0 +1,37 @@
+var request = require('request'),
+    katoss = require('./katoss');
+
+function sendAPICmd(cmd, params, callback) {
+    var apiKey = 'aa28d413d22138d396b018880496c957',
+        port    = '8899',
+        apiCmdUrl = 'https://192.168.1.7:' + port + '/api/' + apiKey + '/?cmd=',
+        url = apiCmdUrl + cmd;
+
+    if (params) {
+        for (var key in params) {
+            if (!params.hasOwnProperty(key)) {
+                continue;
+            }
+            url+= '&' + key + '=' + params[key];
+        }
+    }
+
+    request({
+        url: url,
+        method: 'GET',
+        rejectUnauthorized: false
+    }, function (err, response, body) {
+        if (err) {
+            return console.log(err);
+        }
+        callback(JSON.parse(body));
+    });
+}
+
+// Get show id list
+// ----------------
+sendAPICmd('shows', {'sort': 'name', 'pause': 0}, function (showList) {
+    console.log(showList);
+});
+
+
