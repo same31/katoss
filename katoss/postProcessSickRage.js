3316870289f3b5e95cb668196949c8fd4d27bec8
same31 <matthieu.mitrani@gmail.com>
2015-11-22 22:26:41 +0100

    find srt file

--- katoss/postProcessSickRage.js
+++ katoss/postProcessSickRage.js
@@ -1,3 +1,20 @@
-process.argv.forEach(function (val, index) {
-    console.log(index + ': ' + val);
+
+/*
+2: /volume1/Movies/Séries/South Park/Season 19/South.Park.S19E07.1080p.WEB-DL-YFN.mkv
+3: /volume1/Downloads/South.Park.S19E07.episode.name.1080p.WEB-DL-YFN.mkv
+dirname
+filename
+basename
+*/
+var movieDest = process.argv[2],
+    movieSrc = process.argv[3],
+    fs = require('fs'),
+    pathinfo = require('pathinfo'),
+    find = require('find'),
+    config = require('./config.json'),
+    outputPath = config.outputPath || '.',
+    subSrcBase = pathinfo(movieSrc).basename;
+
+find.file(new RegExp('^' + subSrcBase + '\.[a-zA-Z]{2,3}\.srt$'), outputPath, function(files) {
+    console.log(files);
 });
