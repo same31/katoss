09c93aec940da2749a92044501f83139a3c846dd
same31 <matthieu.mitrani@gmail.com>
2015-12-09 18:51:16 +0100

    + clean Kodi video library function, uses request package + automatically clean kodi library after sick beard post processing if a kodi server is configured in config.json + sampleConfig.json updated + request-promise is not used anymore

---
+++ utils/cleanKodiVideoLibrary.js
@@ -0,0 +1,17 @@
+var request    = require('request'),
+    kodiConfig = require('../katoss/config.json').kodi || {},
+    protocol   = kodiConfig.protocol || 'http',
+    host       = kodiConfig.host || '127.0.0.1',
+    port       = kodiConfig.port || 80,
+    url        = protocol + '://' + host + ':' + port + '/jsonrpc',
+    body       = {
+        jsonrpc: '2.0',
+        id:      1,
+        method:  'VideoLibrary.Clean',
+        params:  { showdialogs: false }
+    };
+
+module.exports = function cleanKodiVideoLibrary (debug) {
+    request(url, { method: 'POST', json: true, body: body });
+    debug && console.log('Clean Kodi video library request sent.');
+};
--- utils/postProcessSickBeard.js
+++ utils/postProcessSickBeard.js
@@ -1,12 +1,13 @@
-var movieDest  = process.argv[2],
-    movieSrc   = process.argv[3],
-    config     = require('../katoss/config.json'),
-    utils      = require('../katoss/utils'),
-    find       = require('find'),
-    fs         = require('fs'),
-    path       = require('path'),
-    outputPath = config.outputPath || '.',
-    subSrcBase = movieSrc.substring(movieSrc.lastIndexOf('/') + 1, movieSrc.lastIndexOf('.'));
+var movieDest             = process.argv[2],
+    movieSrc              = process.argv[3],
+    config                = require('../katoss/config.json'),
+    utils                 = require('../katoss/utils'),
+    cleanKodiVideoLibrary = require('./cleanKodiVideoLibrary').bind(this, true),
+    find                  = require('find'),
+    fs                    = require('fs'),
+    path                  = require('path'),
+    outputPath            = config.outputPath || '.',
+    subSrcBase            = movieSrc.substring(movieSrc.lastIndexOf('/') + 1, movieSrc.lastIndexOf('.'));
 
 find.file(new RegExp('^' + utils.escapeRegExpPattern(subSrcBase) + '.*\.srt$'), path.resolve(outputPath), function (files) {
     if (!files || files.length <= 0) {
@@ -17,5 +18,5 @@ find.file(new RegExp('^' + utils.escapeRegExpPattern(subSrcBase) + '.*\.srt$'), 
         subDest = movieDest.substr(0, movieDest.lastIndexOf('.')) + subExt;
 
     console.log('Move subtitles file from', subSrc, 'to', subDest);
-    fs.rename(subSrc, subDest);
+    fs.rename(subSrc, subDest, config.kodi && cleanKodiVideoLibrary);
 });
