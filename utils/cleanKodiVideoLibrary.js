fce3143740e743522164beeb09b6a82198de0062
same31 <matthieu.mitrani@gmail.com>
2016-06-02 14:25:17 +0200

    v1.4.7: + util updateSickBeardEpisodeStatusFromKodiViews.js + package update

--- utils/cleanKodiVideoLibrary.js
+++ utils/cleanKodiVideoLibrary.js
@@ -1,17 +1,6 @@
-var request    = require('request'),
-    kodiConfig = require('../katoss/config.json').kodi || {},
-    protocol   = kodiConfig.protocol || 'http',
-    host       = kodiConfig.host || '127.0.0.1',
-    port       = kodiConfig.port || 8080,
-    url        = protocol + '://' + host + ':' + port + '/jsonrpc',
-    body       = {
-        jsonrpc: '2.0',
-        id:      1,
-        method:  'VideoLibrary.Clean',
-        params:  { showdialogs: false }
-    };
+var sendKodiAPICmd = require('./sendKodiAPICmd');
 
 module.exports = function cleanKodiVideoLibrary (debug) {
-    request(url, { method: 'POST', json: true, body: body });
+    sendKodiAPICmd('VideoLibrary.Clean', { showdialogs: false });
     debug && console.log('Clean Kodi video library request sent.');
 };
--- utils/updateSickBeardEpisodeStatusFromKodiViews.js
+++ utils/updateSickBeardEpisodeStatusFromKodiViews.js
@@ -1,42 +1,75 @@
 var sendSickBeardAPICmd = require('./sendSickBeardAPICmd'),
-    formatShowNumber    = require('../katoss/src/utils').formatShowNumber;
+    sendKodiAPICmd      = require('./sendKodiAPICmd');
 
-// Retrieve all 'downloaded' episodes from SickBeard and check whether they were viewed in Kodi
-// ============================================================================================
-sendSickBeardAPICmd('shows', { 'sort': 'name', 'paused': 0 }, function (showList) {
-    var searchJSON = {},
-        showName;
-    for (showName in showList) {
-        if (!showList.hasOwnProperty(showName)) {
-            continue;
+sendKodiAPICmd(
+    'VideoLibrary.GetEpisodes',
+    {
+        properties: ['showtitle', 'season', 'episode', 'playcount']
+    },
+    (err, response, body) => {
+        if (err) {
+            console.log(err);
+            return;
         }
-        var show = showList[showName];
-        (function (tvdbid) {
-            sendSickBeardAPICmd('show.seasons', { tvdbid: tvdbid }, function (seasonList) {
-                for (var seasonNumber in seasonList) {
-                    if (!seasonList.hasOwnProperty(seasonNumber)) {
-                        continue;
-                    }
-                    var season      = formatShowNumber(seasonNumber),
-                        episodeList = seasonList[seasonNumber],
-                        episodeNumber;
-                    for (episodeNumber in episodeList) {
-                        if (!episodeList.hasOwnProperty(episodeNumber)) {
-                            continue;
-                        }
-                        var episodeInfo = episodeList[episodeNumber];
-                        if (episodeInfo.status === 'Downloaded') {
-                            searchJSON[showName] || (searchJSON[showName] = { seasons: {}, tvdbid: tvdbid });
 
-                            searchJSON[showName].seasons[season] || (searchJSON[showName].seasons[season] = []);
-                            searchJSON[showName].seasons[season].push(formatShowNumber(episodeNumber));
-                        }
-                    }
+        var kodiEpisodes = body.result.episodes.reduce((kodiEpisodes, episode) => {
+            if (episode.playcount > 0) {
+                kodiEpisodes[episode.showtitle] || (kodiEpisodes[episode.showtitle] = {});
+                kodiEpisodes[episode.showtitle][episode.season] || (kodiEpisodes[episode.showtitle][episode.season] = []);
+                kodiEpisodes[episode.showtitle][episode.season].push(episode.episode);
+            }
+            return kodiEpisodes;
+        }, {});
+
+        // Retrieve all 'downloaded' episodes from SickBeard and check whether they were viewed in Kodi
+        // ============================================================================================
+        sendSickBeardAPICmd('shows', { 'sort': 'name', 'paused': 0 }, function (showList) {
+            var showName;
+            for (showName in showList) {
+                if (!showList.hasOwnProperty(showName) || !kodiEpisodes[showName]) {
+                    continue;
                 }
-            });
-        })(show.tvdbid);
-    }
-    console.log(searchJSON);
-    console.log('\n');
 
-});
+                console.log(showName);
+                var show = showList[showName];
+                (function (tvdbid) {
+                    sendSickBeardAPICmd('show.seasons', { tvdbid: tvdbid }, function (seasonList) {
+                        var seasonNumber;
+                        for (seasonNumber in seasonList) {
+                            if (!seasonList.hasOwnProperty(seasonNumber) || !kodiEpisodes[showName][seasonNumber]) {
+                                continue;
+                            }
+                            console.log(seasonNumber + ':');
+                            var episodeList = seasonList[seasonNumber],
+                                episodeNumber,
+                                episodeInfo;
+                            for (episodeNumber in episodeList) {
+                                if (!episodeList.hasOwnProperty(episodeNumber) || !~kodiEpisodes[showName][seasonNumber].indexOf(parseInt(episodeNumber))) {
+                                    continue;
+                                }
+                                episodeInfo = episodeList[episodeNumber];
+
+                                if (episodeInfo.status === 'Downloaded') {
+                                    console.log(episodeNumber);
+                                    // 'archived' status is not available in the API anymore...
+                                    sendSickBeardAPICmd(
+                                        'episode.setstatus',
+                                        {
+                                            'tvdbid':  tvdbid,
+                                            'season':  parseInt(seasonNumber),
+                                            'episode': parseInt(episodeNumber),
+                                            'status':  'skipped',
+                                            'force':   1
+                                        }
+                                    );
+                                }
+                                else if (!episodeInfo.status) {
+                                    console.log(episodeNumber, 'status not retrieved');
+                                }
+                            }
+                        }
+                    });
+                })(show.tvdbid);
+            }
+        });
+    });
