82296a19b972cef4f29da2bb341e5f0eb4452325
same31 <matthieu.mitrani@gmail.com>
2016-07-28 11:53:38 +0200

    rarbg api fixed

--- katoss/src/torrentProviders/rarbg.js
+++ katoss/src/torrentProviders/rarbg.js
@@ -1,37 +1,12 @@
-var utils      = require('./../../src/utils'),
-    Torrentapi = require('rarbgto-api'),
-    rarbg      = new Torrentapi('katoss');
+var utils = require('./../../src/utils'),
+    rarbg = require('rarbgto-api');
 
 function searchEpisode (show, season, episode) {
-    return rarbg.search({
-        search_string: utils.formatShowTitle(show) + ' S' + season + 'E' + episode,
-        category:      'tv',
-        limit:         100,
-        sort:          'seeders',
-        min_seeders:   1,
-        format:        'json_extended',
-        ranked:        0
-    }).then(torrentList => torrentList.map(torrentInfo => {
-        torrentInfo.title = torrentInfo.title.trim();
-        torrentInfo.seeds = torrentInfo.seeders;
-        delete torrentInfo.seeders;
-        return torrentInfo;
-    }));
+    return rarbg.search(utils.formatShowTitle(show) + ' S' + season + 'E' + episode);
 }
 
 function extractTorrentFilenameAndUrl (torrentInfo) {
-    var filename   = torrentInfo.title + '.torrent',
-        magnetHash = torrentInfo.download.match(/[0-9a-f]{40}/i),
-        url;
-
-    if (magnetHash && magnetHash.length) {
-        url = 'http://itorrents.org/torrent/' + magnetHash[0].toUpperCase() + '.torrent?title=' + encodeURIComponent(torrentInfo.title);
-    }
-
-    return {
-        filename: filename,
-        url:      url || ''
-    };
+    return torrentInfo;
 }
 
 module.exports = {
