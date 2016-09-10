a1a24c026d0c809816a9a090bf24491817115b4c
same31 <matthieu.mitrani@gmail.com>
2016-09-10 19:49:22 +0200

    typo

--- katoss/src/torrentProviders/rarbg.js
+++ katoss/src/torrentProviders/rarbg.js
@@ -2,7 +2,6 @@ var utils = require('../../src/utils'),
     rarbg = require('../../../utils/rarbgto-api.min');
 
 function searchEpisode (show, season, episode) {
-    console.log('here');
     return rarbg.search(utils.formatShowTitle(show) + ' S' + season + 'E' + episode, {
         category: 'tv',
         order:    'seeders',
