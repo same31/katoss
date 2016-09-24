ce7e8b8ebdb203ddabb40b43a022b5cec82d7d07
same31 <matthieu.mitrani@gmail.com>
2016-09-24 12:27:50 +0200

    new rarbg bundle

--- katoss/src/torrentProviders/rarbg.js
+++ katoss/src/torrentProviders/rarbg.js
@@ -1,5 +1,5 @@
 var utils = require('../../src/utils'),
-    rarbg = require('../../../lib/rarbgto-api.min.js');
+    rarbg = require('../../../lib/rarbgto-api.min');
 
 function searchEpisode (show, season, episode) {
     return rarbg.search(utils.formatShowTitle(show) + ' S' + season + 'E' + episode, {
